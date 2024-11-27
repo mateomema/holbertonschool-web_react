9b0f87acb7aba47c5dd0a74c416b97c50ef45392
mateomema <mateomema1@gmail.com.com>
2024-11-27 17:28:29 +0100

    ok

---
+++ OneDrive/Desktop/Holberton Full Stack/holberton-machine_learning/3-first_launch.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""
+Uses the (unofficial) SpaceX API to print the upcoming launch as:
+<launch name> (<date>) <rocket name> - <launchpad name> (<launchpad locality>)
+
+The “upcoming launch” is the one which is the soonest from now, in UTC
+and if 2 launches have the same date, it's the first one in the API result.
+"""
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.spacexdata.com/v4/launches/upcoming"
+    results = requests.get(url).json()
+    dateCheck = float('inf')
+    launchName = None
+    rocket = None
+    launchPad = None
+    location = None
+    for launch in results:
+        launchDate = launch.get('date_unix')
+        if launchDate < dateCheck:
+            dateCheck = launchDate
+            date = launch.get('date_local')
+            launchName = launch.get('name')
+            rocket = launch.get('rocket')
+            launchPad = launch.get('launchpad')
+    if rocket:
+        url = 'https://api.spacexdata.com/v4/rockets/{}'
+        rocket = requests.get(url.format(rocket)).json().get('name')
+    if launchPad:
+        launchpads_url = 'https://api.spacexdata.com/v4/launchpads/{}'
+        launchpad = requests.get(launchpads_url.format(launchPad)).json()
+        launchPad = launchpad.get('name')
+        location = launchpad.get('locality')
+
+    print("{} ({}) {} - {} ({})".format(
+        launchName, date, rocket, launchPad, location))
